3e6dc402571d393d4ff76a2a56bbeec811b4ab92
Ubuntu <ubuntu@ip-172-31-14-117.ec2.internal>
2020-11-27 17:40:58 +0000

    Add name age to user

--- db/migrate/20201127172218_add_name_and_age.rb
+++ db/migrate/20201127172218_add_name_and_age.rb
@@ -1,7 +1,9 @@
 class AddNameAndAge < ActiveRecord::Migration[6.0]
-  def change
+  
+    def change
     add_column :users, :name, :string
     add_column :users, :age, :integer
     add_index :users, :name, unique: true
-  end
+  
+    end
 end
