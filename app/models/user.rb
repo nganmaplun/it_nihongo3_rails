35737314bb820139f4e091330aa059b1044dd0ab
Ubuntu <ubuntu@ip-172-31-14-117.ec2.internal>
2020-11-28 16:03:58 +0000

    Modify name to review list

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_one_attached :avatar
+    has_one_attached :avataras
+    # has_many :book_review
   validates :name, presence: true
   validates :age, presence: true
 end
