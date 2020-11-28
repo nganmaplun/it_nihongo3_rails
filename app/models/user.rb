a40caa96a152eccd35599f599cf63b6150e81e42
Ubuntu <ubuntu@ip-172-31-14-117.ec2.internal>
2020-11-28 16:33:05 +0000

    Fix_bug_add_age_and_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_one_attached :avataras
+    has_one_attached :avatar
     # has_many :book_review
   validates :name, presence: true
   validates :age, presence: true
